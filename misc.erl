da247c3cb3085ba0bb94d8b4822139359f430b16
Rafael Caricio <rafael.jacinto@gmail.com>
2014-12-20 18:29:23 +0100

    Some experimental

---
+++ misc.erl
@@ -0,0 +1,19 @@
+-module(misc).
+-export([factorial/1, member/2, len/1, index_of/2]).
+
+factorial(0) -> 1;
+factorial(N) -> N * factorial(N - 1).
+
+member(H, [H|_]) -> true;
+member(H, [_|T]) -> member(H, T);
+member(_, []) -> false.
+
+len([]) -> 0;
+len([_|L]) -> 1 + len(L).
+
+index_of(_, []) -> -1;
+index_of(E, L) -> index_of(E, L, 0).
+
+index_of(E, [E|_], Pos) -> Pos;
+index_of(E, [_|L], Pos) -> index_of(E, L, Pos + 1);
+index_of(_, [], _) -> -1.
