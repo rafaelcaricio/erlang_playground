48662c505843ac31c29cdaf3b59c64ae242fa8dc
Rafael Caricio <rafael.jacinto@gmail.com>
2014-12-20 15:23:59 +0100

    Little simplification

--- reverse_words.erl
+++ reverse_words.erl
@@ -12,11 +12,8 @@ split([E|L], E, Res, Current) -> Res ++ [Current] ++ split(L, E);
 split([E|L], P, Res, Current) -> split(L, P, Res, Current ++ [E]).
 
 join([], _) -> [];
-join(List, Pattern) -> join(List, Pattern, []).
-
-join([], _, Result) -> Result;
-join([Element|[]], _, _) -> Element;
-join([Element|List], Pattern, _) -> Element ++ Pattern ++ join(List, Pattern).
+join([Element|[]], _) -> Element;
+join([Element|List], Pattern) -> Element ++ Pattern ++ join(List, Pattern).
 
 main(_) ->
   io:fwrite(join(reverse(split(reverse("that's something very interesting"), $ )), " ")),
