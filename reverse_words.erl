f1395d04fca30ee555cd279d4bf04fc81a7a14f4
Rafael Caricio <rafael.jacinto@gmail.com>
2014-12-20 20:50:50 +0100

    Some small refactor

--- reverse_words.erl
+++ reverse_words.erl
@@ -1,5 +1,7 @@
 #!/usr/bin/env escript
 %% -*- erlang -*-
+-module(reverse_words).
+-export([reverse_words/1]).
 
 reverse([]) -> [];
 reverse(List) -> reverse(List, []).
@@ -18,6 +20,8 @@ join([], _) -> [];
 join([Element|[]], _) -> Element;
 join([Element|List], Pattern) -> [[Element|Pattern]|join(List, Pattern)].
 
+reverse_words(Sentence) -> join(reverse(split(reverse(Sentence), $ )), " ").
+
 main(_) ->
-  Result = join(reverse(split(reverse("that's something very interesting"), $ )), " "),
+  Result = reverse_words("that's something very interesting"),
   io:format("~s~n", [Result]).
