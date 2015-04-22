bfc4ddc0852a5927887aa68ec33ec70221d31ce7
Rafael Caricio <rafael.jacinto@gmail.com>
2015-04-22 23:59:56 +0200

    add db_server

--- db_server.erl
+++ db_server.erl
@@ -1,11 +1,49 @@
 -module(db_server).
 
--export([start/0, loop/1]).
+-export([start/0, loop/1, stop/0, write/2, read/1, delete/1, match/1]).
 
 
 start() ->
     DbRef = db:new(),
-    spawn_link(?MODULE, loop, [DbRef]).
+    Pid = spawn_link(?MODULE, loop, [DbRef]),
+    register(db_server_process, Pid).
+
+
+stop() ->
+    db_server_process ! {self(), stop},
+    receive
+        {ok, _} ->
+            ok
+        after 100 ->
+            timeout
+    end.
+
+
+write(Key, Element) ->
+    db_server_process ! {self(), {write, {Key, Element}}},
+    receive_result().
+
+
+read(Key) ->
+    db_server_process ! {self(), {read, Key}},
+    receive_result().
+
+
+delete(Key) ->
+    db_server_process ! {self(), {delete, Key}},
+    receive_result().
+
+
+match(Element) ->
+    db_server_process ! {self(), {match, Element}},
+    receive_result().
+
+
+receive_result() ->
+    receive
+        Result -> Result
+        after 100 -> timeout
+    end.
 
 
 loop(DbRef) ->
@@ -13,9 +51,18 @@ loop(DbRef) ->
         {Client, {write, Args}} ->
             {Key, Data} = Args,
             NewDbRef = db:write(Key, Data, DbRef),
-            Client ! {self(), ok},
+            Client ! {ok},
             loop(NewDbRef);
-        {Client, {read, KeyToRead}} ->
-            Client ! {self(), db:read(KeyToRead, DbRef)}
+        {Client, {read, Key}} ->
+            Client ! db:read(Key, DbRef);
+        {Client, {delete, Key}} ->
+            NewDbRef = db:delete(Key, DbRef),
+            Client ! {ok},
+            loop(NewDbRef);
+        {Client, {match, Element}} ->
+            Client ! {ok, db:match(Element, DbRef)};
+        {Client, stop} ->
+            Client ! {ok, stopped},
+            exit(self(), client_stop)
     end,
     loop(DbRef).
